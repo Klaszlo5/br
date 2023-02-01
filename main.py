2b4ed8525065cbca364eb2bf3bb710e18dfea875
Klaszlo5 <gplays131@gmail.com>
2023-02-01 21:40:57 +0100

    eredmeny -1,2

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #megoldas
-def pontok(lap)->int:
-    pontok:int = 0
-    for i in range(len(lap)):
-        pontok += lap[i];
-    return pontok
-
+def eredmeny(egy, ketto):
+    napok = pontok(egy)
+    jatekos = pontok(ketto)
+    for jatekos in range(napok):
+        napok[jatekos] += napok[jatekos]
+    if napok > 21 and jatekos > 21:
+        print("nyer")
+    elif napok > 21:
+        return "Gép vesztett"
+    else:
+        return "Játékos vesztett"
 def osszeg(cds: [int]) -> int:
     pts: int = 0
     for i in range(len(cds)):
